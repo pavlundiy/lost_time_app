0ce03ff047ffd54fa47507bb373d98a016560140
pavlundiy <36733082+pavlundiy@users.noreply.github.com>
2026-04-03 14:35:00 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = 'quotes-app-v1';
+const ASSETS_TO_CACHE = [
+  './Мотивирующие Цитаты.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&family=Playfair+Display:ital,wght@1,500&display=swap',
+  'https://html2canvas.hertzen.com/dist/html2canvas.min.js'
+];
+
+// Установка Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Кэширование активов');
+      return cache.addAll(ASSETS_TO_CACHE).catch(err => {
+        console.log('Некоторые ресурсы не закэшировались:', err);
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+// Активация Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Удаление старого кэша:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Перехват запросов
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
+    }).catch(() => {
+      // Офлайн - возвращаем главную страницу
+      return caches.match('./Мотивирующие Цитаты.html');
+    })
+  );
+});
